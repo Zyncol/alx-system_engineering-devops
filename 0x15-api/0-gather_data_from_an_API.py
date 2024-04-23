ed715d02be08d43d0a6a0fa6c3e704a465eafd39
Zyncol <isaacyilipochipofya@gmail.com>
2024-04-23 04:58:07 +0200

    to do list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-TODO list progress"""
+"""
+a script that, using a REST API, for a given employee ID,
+returns information about TODO list progress.
+"""
 
-import requests
-import sys
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    feedback = get('https://jsonplaceholder.typicode.com/users')
+    sdata = feedback.json()
 
-    uId = sys.argv[1]
-    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                       .format(uId))
-    name = usr.json().get('name')
+    for e in sdata:
+        if e.get('id') == int(argv[1]):
+            employee = e.get('name')
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    tTask = 0
-    donee = 0
+    for e in data:
+        if e.get('userId') == int(argv[1]):
+            total += 1
 
-    for task in todos.json():
-        if task.get('uId') == int(uId):
-            tTask += 1
-            if task.get('donee'):
-                donee += 1
+            if e.get('completed') is True:
+                completed += 1
+                tasks.append(e.get('title'))
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, donee, tTask))
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
 
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('uId') == int(uId) and task.get('donee')]))
+    for e in tasks:
+        print("\t {}".format(e))
